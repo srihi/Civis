906108b6497392a20ec5a3098ece142e7f151742
Anh.Hoang <h.anh.viet91@gmail.com>
2017-06-22 17:17:39 +0300

    added api skeleton & updated manifest

---
+++ app/src/main/java/com/smarty/civis/activities/sync/DigitalTownApiService.java
@@ -0,0 +1,40 @@
+package com.smarty.civis.activities.sync;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
+
+/**
+ * Created by Anh.Hoang on 6/22/2017.
+ */
+
+public class DigitalTownApiService {
+    private static DigitalTownApiService INSTANCE;
+
+    private static final String URL = "";
+    private Retrofit mRetrofit;
+    private IDigitalTownApi mDigitalTownApi;
+
+    private DigitalTownApiService(){
+        mRetrofit = new Retrofit
+                .Builder()
+                .baseUrl(URL)
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build();
+
+        mDigitalTownApi = mRetrofit.create(IDigitalTownApi.class);
+    }
+
+
+    public static DigitalTownApiService getInstance(){
+        if(INSTANCE == null){
+            INSTANCE = new DigitalTownApiService();
+        }
+
+        return INSTANCE;
+    }
+
+
+    interface IDigitalTownApi{
+
+    }
+}
