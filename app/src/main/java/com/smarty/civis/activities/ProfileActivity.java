885101b9a023a6e06e3fc1cf20461862c3198d90
ditek <diaa.33@gmail.com>
2017-06-24 04:21:36 +0200

    ProfileActivity: Add activity and layout

---
+++ app/src/main/java/com/smarty/civis/activities/ProfileActivity.java
@@ -0,0 +1,47 @@
+package com.smarty.civis.activities;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.smarty.civis.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.nav_profile:
+                    mTextMessage.setText(R.string.title_profile);
+                    return true;
+                case R.id.nav_jobs_performed:
+                    mTextMessage.setText(R.string.title_performed);
+                    return true;
+                case R.id.nav_jobs_offered:
+                    mTextMessage.setText(R.string.title_offered);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
