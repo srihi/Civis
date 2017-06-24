a730c467e07ed75261b34c8cb1eeb3d40aff5ca0
ditek <diaa.33@gmail.com>
2017-06-25 01:31:28 +0200

    ProfileActivity: Add profile fragment

--- app/src/main/java/com/smarty/civis/activities/ProfileActivity.java
+++ app/src/main/java/com/smarty/civis/activities/ProfileActivity.java
@@ -3,15 +3,48 @@ package com.smarty.civis.activities;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.smarty.civis.R;
+import com.smarty.civis.fragments.ProfileFragment;
 
-public class ProfileActivity extends AppCompatActivity {
+public class ProfileActivity extends FragmentActivity {
 
     private TextView mTextMessage;
+//    final Fragment profileFragment = ProfileFragment/.newInstance("1", "2");
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        if (findViewById(R.id.fragment_container) != null) {
+            // Check if we're being restored from a previous state
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            ProfileFragment profileFragment = new ProfileFragment();
+
+            // Pass the Intent's extras to the fragment as arguments
+            profileFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.fragment_container, profileFragment).commit();
+        }
+    }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -20,7 +53,11 @@ public class ProfileActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.nav_profile:
-                    mTextMessage.setText(R.string.title_profile);
+                    ProfileFragment profileFragment = new ProfileFragment();
+                    FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.fragment_container, profileFragment);
+                    transaction.addToBackStack(null);
+                    transaction.commit();
                     return true;
                 case R.id.nav_jobs_performed:
                     mTextMessage.setText(R.string.title_performed);
@@ -33,15 +70,5 @@ public class ProfileActivity extends AppCompatActivity {
         }
 
     };
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-    }
 
 }
