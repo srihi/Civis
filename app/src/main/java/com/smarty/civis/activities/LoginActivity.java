c91d59752f0bf41d4e05dacd56471f53e0e840df
ditek <diaa.33@gmail.com>
2017-06-27 02:03:22 +0200

    - Implement login logic - Add the option to skip login

--- app/src/main/java/com/smarty/civis/activities/LoginActivity.java
+++ app/src/main/java/com/smarty/civis/activities/LoginActivity.java
@@ -13,35 +13,48 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.smarty.civis.R;
 import com.smarty.civis.utils.AuthUtils;
+import com.smarty.civis.utils.PrefUtils;
 
 /**
  * A login screen that offers login via email/password.
  */
 public class LoginActivity extends AppCompatActivity {
 
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-    private UserLoginTask mAuthTask = null;
+    private final String LOG_TAG = LoginActivity.class.getSimpleName();
+
+//    /**
+//     * A dummy authentication store containing known user names and passwords.
+//     * TODO: remove after connecting to a real authentication system.
+//     */
+//    private static final String[] DUMMY_CREDENTIALS = new String[]{
+//            "foo@example.com:hello", "bar@example.com:world"
+//    };
+//    /**
+//     * Keep track of the login task to ensure we can cancel it if requested.
+//     */
+//    private UserLoginTask mAuthTask = null;
 
     // UI references.
     private AutoCompleteTextView mEmailView;
     private EditText mPasswordView;
     private View mLoginOverlay;
 
+    private boolean mLoginRequested = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // If we are already authorized go to MainActivity directly
+        if(!TextUtils.isEmpty(PrefUtils.getCode(this))){
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }
+
         setContentView(R.layout.activity_login);
         // Set up the login form.
         mLoginOverlay = findViewById(R.id.login_overlay);
@@ -60,20 +73,26 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    public void skipLogin(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
     /**
      * Attempts to log in.
      * If there are form errors (invalid email, missing fields, etc.), the
      * errors are presented and no actual login attempt is made.
      */
     public void attemptLogin(View view) {
+        mLoginRequested = true;
+        Intent intent = AuthUtils.getAuthenticationIntent();
+        startActivity(intent);
+
+    /*
         if (mAuthTask != null) {
             return;
         }
 
-        Intent intent = AuthUtils.getAuthenticationIntent();
-        startActivity(intent);
-
-        /*
         // Reset errors.
         mEmailView.setError(null);
         mPasswordView.setError(null);
@@ -117,91 +136,100 @@ public class LoginActivity extends AppCompatActivity {
         */
     }
 
-    private boolean isEmailValid(String email) {
-        //Simple email validity check
-        return email.contains("@");
-    }
-
-    private boolean isPasswordValid(String password) {
-        //Simple email password check
-        return password.length() > 4;
-    }
-
-    /**
-     * Shows the progress UI
-     */
-    private void showProgress(final boolean show) {
-        int animTime = getResources().getInteger(android.R.integer.config_longAnimTime);
-        mLoginOverlay.setVisibility(show ? View.VISIBLE : View.GONE);
-        mLoginOverlay.animate().setDuration(animTime).alpha(show ? (float) 0.5 : 0);
-    }
-
-    public void skipLogin(View view) {
-    }
-
-    /**
-     * Represents an asynchronous login/registration task used to authenticate
-     * the user.
-     */
-    private class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-
-        private final String mEmail;
-        private final String mPassword;
-
-        UserLoginTask(String email, String password) {
-            mEmail = email;
-            mPassword = password;
-        }
-
-        @Override
-        protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-
-            try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-
-            for (String credential : DUMMY_CREDENTIALS) {
-                String[] pieces = credential.split(":");
-                if (pieces[0].equals(mEmail)) {
-                    // Account exists, return true if the password matches.
-                    return pieces[1].equals(mPassword);
-                }
-            }
-
-            return false;
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success) {
-            mAuthTask = null;
-            showProgress(false);
-
-            if (success) {
-                finish();
-            } else {
-                mPasswordView.setError(getString(R.string.error_incorrect_password));
-                mPasswordView.requestFocus();
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            mAuthTask = null;
-            showProgress(false);
-        }
-    }
+//    private boolean isEmailValid(String email) {
+//        //Simple email validity check
+//        return email.contains("@");
+//    }
+//
+//    private boolean isPasswordValid(String password) {
+//        //Simple email password check
+//        return password.length() > 4;
+//    }
+//
+//    /**
+//     * Shows the progress UI
+//     */
+//    private void showProgress(final boolean show) {
+//        int animTime = getResources().getInteger(android.R.integer.config_longAnimTime);
+//        mLoginOverlay.setVisibility(show ? View.VISIBLE : View.GONE);
+//        mLoginOverlay.animate().setDuration(animTime).alpha(show ? (float) 0.5 : 0);
+//    }
+//
+//    /**
+//     * Represents an asynchronous login/registration task used to authenticate
+//     * the user.
+//     */
+//    private class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
+//
+//        private final String mEmail;
+//        private final String mPassword;
+//
+//        UserLoginTask(String email, String password) {
+//            mEmail = email;
+//            mPassword = password;
+//        }
+//
+//        @Override
+//        protected Boolean doInBackground(Void... params) {
+//            // TODO: attempt authentication against a network service.
+//
+//            try {
+//                // Simulate network access.
+//                Thread.sleep(2000);
+//            } catch (InterruptedException e) {
+//                return false;
+//            }
+//
+//            for (String credential : DUMMY_CREDENTIALS) {
+//                String[] pieces = credential.split(":");
+//                if (pieces[0].equals(mEmail)) {
+//                    // Account exists, return true if the password matches.
+//                    return pieces[1].equals(mPassword);
+//                }
+//            }
+//
+//            return false;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(final Boolean success) {
+//            mAuthTask = null;
+//            showProgress(false);
+//
+//            if (success) {
+//                finish();
+//            } else {
+//                mPasswordView.setError(getString(R.string.error_incorrect_password));
+//                mPasswordView.requestFocus();
+//            }
+//        }
+//
+//        @Override
+//        protected void onCancelled() {
+//            mAuthTask = null;
+//            showProgress(false);
+//        }
+//    }
 
     @Override
     protected void onResume() {
         super.onResume();
+        boolean success = false;
         Uri uri = getIntent().getData();
         if (uri != null){
             String code = uri.getQueryParameter("code");
-            Log.i("", code);
+            Log.i(LOG_TAG, "Code URI " + uri);
+            if(code != null){
+                success = true;
+                PrefUtils.putCode(this, uri);
+                startActivity(new Intent(this, MainActivity.class));
+                finish();
+            }
+            Log.i(LOG_TAG, "Authorization code: " + code);
+        }
+        if(!success && mLoginRequested){
+            mLoginRequested = false;
+            Toast.makeText(this, R.string.login_failed, Toast.LENGTH_SHORT).show();
         }
     }
 }
--- app/src/main/java/com/smarty/civis/utils/PrefUtils.java
+++ app/src/main/java/com/smarty/civis/utils/PrefUtils.java
@@ -8,6 +8,8 @@ import android.text.TextUtils;
 
 import com.smarty.civis.R;
 
+import static android.R.attr.data;
+
 /**
  * Created by anh.hoang on 6/24/17.
  */
@@ -15,8 +17,7 @@ import com.smarty.civis.R;
 public class PrefUtils {
     private static final String CODE_PARAM = "code";
 
-    public static void putToken(Context context, Uri data) {
-        String token = data.getQueryParameter(CODE_PARAM);
+    public static void putToken(Context context, String token) {
         if (!TextUtils.isEmpty(token)) {
             SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
             SharedPreferences.Editor editor = preferences.edit();
@@ -28,5 +29,21 @@ public class PrefUtils {
     public static String getToken(Context context) {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
         return preferences.getString(context.getString(R.string.pref_access_token_key), null);
+    }
+
+    // Store authorization code in the preferences
+    public static void putCode(Context context, Uri data) {
+        String code = data.getQueryParameter(CODE_PARAM);
+        if (!TextUtils.isEmpty(code)) {
+            SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+            SharedPreferences.Editor editor = preferences.edit();
+            editor.putString(context.getString(R.string.pref_code_key), code);
+            editor.commit();
+        }
+    }
+
+    public static String getCode(Context context) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        return preferences.getString(context.getString(R.string.pref_code_key), null);
     }
 }
